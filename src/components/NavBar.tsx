4245f85d9d1f9f3b41658682ce0628abd6746fff
Vinicius Costa <vinicius.costa89@gmail.com>
2022-04-27 20:26:48 -0300

    refactor(navbar): change directory and some cleaning

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,45 +11,37 @@ interface ItemsData {
 	label: string
 }
 
-export const NavBar = ({ items, navId, ...props }: NavBarProps) => {	
+export const NavBar = ({ items, navId }: NavBarProps) => {
 	const [isMenuOpen, setMenuOpen] = useState(false);
 
 	const toggleMenu = () => {
 		document.body.classList.toggle('menu-opened');
 		setMenuOpen(!isMenuOpen);
-	}
+	};
 
 	return (
 		<>
-			<button 
-				className={`hamburger ${isMenuOpen ? 'hamburger--active' : ''}`}
-				onClick={toggleMenu}
-			>
+			<button className={`hamburger ${isMenuOpen ? 'hamburger--active' : ''}`} onClick={toggleMenu}>
 				<span className="hamburger__line"></span>
 				<span className="hamburger__line"></span>
 				<span className="hamburger__line"></span>
 			</button>
 
-			<div className={`navbar__menu ${isMenuOpen ? 'navbar__menu--opened' : ''}`}>		
-				<nav className="navbar" {...props}>
+			<div className={`navbar__menu ${isMenuOpen ? 'navbar__menu--opened' : ''}`}>
+				<nav className="navbar">
 					<ul className="navbar__list">
-
-						{	items.map((item, i) => (
+						{items.map((item, i) => (
 							<li className="navbar__item" key={i}>
-								<a 
-									className={`navbar__link ${item.id === navId ? 'navbar__link--active' : ''}`}
-									href={item.path}								
-								>
+								<a className={`navbar__link ${item.id === navId ? 'navbar__link--active' : ''}`} href={item.path}>
 									<strong className="navbar__number">0{i}</strong>
 									<span className="navbar__label">{item.label}</span>
-								</a>	
+								</a>
 							</li>
-						))	}
-
+						))}
 					</ul>
 				</nav>
 			</div>
 		</>
-	)
-}
+	);
+};
 
